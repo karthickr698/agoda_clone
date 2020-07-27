22ae75363e2228555806418052e6e50dd1e620de
nikmat1996 <nikhilmathew1996@gmail.com>
2020-07-27 19:10:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Navbar from './components/common/Navbar';
 import LandingPageNav from './components/landingPageComponents/LandingPageNav';
 import ExploreMore from './components/landingPageComponents/ExploreMore';
