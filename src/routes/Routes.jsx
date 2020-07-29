b98c979d49d5718f34a4a04f2d23748ccb99518d
vinaykanth-edu <vinaykanth.edu@gmail.com>
2020-07-29 21:16:11 +0530

    payment page added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,12 +8,14 @@ import ExploreMore from '../components/landingPageComponents/ExploreMore';
 import RecommendedPlaces from '../components/landingPageComponents/RecommendedPlaces';
 import ApartmentsPage from '../components/entityPageComponents/ApartmentsPage.jsx'
 import EntityPage from '../components/entityPageComponents/EntityPage';
+import PaymentPage from '../components/paymentPageComponents/PaymentPage';
 
 export default function Routes() {
     return (
         <div>
             <Navbar />
-            <EntityPage />
+            {/* <EntityPage /> */}
+            <PaymentPage />
             {/* <Switch>
                 <Route exact path='/' render={() => <>
                     <ExploreMore />
