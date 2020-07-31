53123dd98de30db386bf732a3ff9871f213fb575
nikmat1996 <nikhilmathew1996@gmail.com>
2020-07-31 16:37:16 +0530

    test

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -25,6 +25,7 @@ export default function Routes() {
                 <Route path='/paymentPage' component={PaymentPage} />
             </Switch>
             <Footer />
+            <div>test</div>
         </div>
     )
 }
